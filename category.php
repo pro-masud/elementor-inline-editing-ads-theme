c919c356e98b7d2761ea4cd14767851e68368c92
pro-mannan <promasudbd@gmail.com>
2023-11-08 11:51:58 +0600

    theme categorys archive system working today successfully complele

---
+++ category.php
@@ -0,0 +1,44 @@
+<!-- include header file  -->
+<?php get_header(); ?>
+
+        <?php
+            get_template_part('hero');
+        ?>
+
+        <!-- blog section start -->
+        <section class="masonary-blog-section ptb-120">
+            <div class="container">
+                <div class="row">
+                    
+                
+               <h1> This is a Current Categorys Name:<?php single_cat_title(); ?></h1>
+                </div>
+                <div class="row">
+                    <?php 
+                        while(have_posts()){
+                            the_post();
+
+                           ?>
+                           <h2><a href="<?php the_permalink(); ?>"> <?php the_title(); ?></a></h2>
+                           <?php 
+                        } 
+                    ?>
+                </div>
+                <div class="row">
+                    <div class="col-md-4 col-lg-6">
+                        <?php 
+                             the_posts_pagination( array(
+                                'title'              => '', // this should hide the title
+                                'prev_text'          => __( 'Previous', 'twentyfifteen' ),
+                                'next_text'          => __( 'Next', 'twentyfifteen' ),
+                                'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( '', 'nieuwedruk' ) . ' </span>',
+                            ) );
+                        
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- blog section end -->
+
+<?php get_footer(); ?>
